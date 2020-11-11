edabc4534dee0a4174ce654c435bf703587d12f1
alaasahraoui <42116469+alaasahraoui@users.noreply.github.com>
2020-11-11 12:13:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ import random
 import pandas as pd
 import os
 from time import sleep
+
+import smtplib,ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.utils import formatdate
+from email import encoders
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import datetime
+import random
+import pandas as pd
+import os
+from time import sleep
+import smtplib
+
+
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options = webdriver.ChromeOptions()
@@ -44,3 +62,36 @@ os.system("git push -u origin main --force")
 print('pushed')
 
 driver.quit()
+def send_an_email():
+    toaddr = 'sahraoui.alaaeddine@gmail.com'    
+    me = 'sahraoui.alaaeddine@gmail.com' 
+    subject = "What's News"
+
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = me
+    msg['To'] = toaddr
+    msg.preamble = "test " 
+    #msg.attach(MIMEText(text))
+
+    part = MIMEBase('application', "octet-stream")
+    part.set_payload(open("questions.csv", "rb").read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="questions.csv"')
+    msg.attach(part)
+
+    try:
+       s = smtplib.SMTP('smtp.gmail.com', 587)
+       s.ehlo()
+       s.starttls()
+       s.ehlo()
+       s.login(user = 'sahraoui.alaaeddine@gmail.com', password = 'ksvablrpdktsqabt')
+       #s.send_message(msg)
+       s.sendmail(me, toaddr, msg.as_string())
+       s.quit()
+    #except:
+    #   print ("Error: unable to send email")
+    except SMTPException as error:
+          print ("Error")
+
+send_an_email()
